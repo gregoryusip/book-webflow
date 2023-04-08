844f097cb37cb8131489ea064b2c3dfe09edefa2
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-04-08 13:37:57 +0700

    feat: navbar & footer component

---
+++ components/Footer.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="footer">
+    <div class="footer--container">
+      <div class="footer--container__wrapper w-full md:w-1/2 lg:w-[23%]">
+        <div class="footer--pages">
+          <img
+            src="@/assets/img/logo/logo.png"
+            alt="Logo"
+            class="footer--pages__logo"
+          />
+          <div class="footer--pages__socmed">
+            <div class="socmed socmed-border-yellow">
+              <svg
+                width="10"
+                height="17"
+                viewBox="0 0 10 17"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.71875 9.5H6.375V16.5H3.25V9.5H0.71875V6.59375H3.25V4.40625C3.25 3.15625 3.59375 2.19792 4.28125 1.53125C4.96875 0.84375 5.88542 0.5 7.03125 0.5C7.36458 0.5 7.71875 0.520833 8.09375 0.5625C8.46875 0.583333 8.76042 0.614583 8.96875 0.65625L9.28125 0.6875V3.15625H8.03125C7.44792 3.15625 7.02083 3.3125 6.75 3.625C6.5 3.91667 6.375 4.28125 6.375 4.71875V6.59375H9.15625L8.71875 9.5Z"
+                />
+              </svg>
+            </div>
+            <div class="socmed socmed-border-yellow">
+              <svg
+                width="16"
+                height="13"
+                viewBox="0 0 16 13"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.3438 3.25C14.3646 3.33333 14.375 3.46875 14.375 3.65625C14.375 4.76042 14.1667 5.86458 13.75 6.96875C13.3333 8.05208 12.7396 9.05208 11.9688 9.96875C11.2188 10.8646 10.2396 11.5938 9.03125 12.1562C7.82292 12.7188 6.48958 13 5.03125 13C3.19792 13 1.52083 12.5104 0 11.5312C0.229167 11.5521 0.489583 11.5625 0.78125 11.5625C2.30208 11.5625 3.66667 11.0938 4.875 10.1562C4.14583 10.1562 3.5 9.94792 2.9375 9.53125C2.39583 9.09375 2.02083 8.55208 1.8125 7.90625C2.02083 7.92708 2.21875 7.9375 2.40625 7.9375C2.69792 7.9375 2.98958 7.90625 3.28125 7.84375C2.53125 7.67708 1.90625 7.29167 1.40625 6.6875C0.90625 6.08333 0.65625 5.39583 0.65625 4.625V4.5625C1.11458 4.83333 1.60417 4.97917 2.125 5C1.14583 4.33333 0.65625 3.41667 0.65625 2.25C0.65625 1.66667 0.8125 1.11458 1.125 0.59375C1.9375 1.61458 2.92708 2.42708 4.09375 3.03125C5.28125 3.63542 6.54167 3.96875 7.875 4.03125C7.83333 3.78125 7.8125 3.53125 7.8125 3.28125C7.8125 2.38542 8.125 1.61458 8.75 0.96875C9.39583 0.322917 10.1667 0 11.0625 0C12.0208 0 12.8229 0.34375 13.4688 1.03125C14.2188 0.885417 14.9167 0.625 15.5625 0.25C15.3125 1.02083 14.8333 1.625 14.125 2.0625C14.75 1.97917 15.375 1.80208 16 1.53125C15.5417 2.19792 14.9896 2.77083 14.3438 3.25Z"
+                />
+              </svg>
+            </div>
+            <div class="socmed socmed-border-yellow">
+              <svg
+                width="14"
+                height="15"
+                viewBox="0 0 14 15"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.125 14.5H0.21875V5.15625H3.125V14.5ZM2.875 3.375C2.54167 3.70833 2.14583 3.875 1.6875 3.875C1.22917 3.875 0.833333 3.70833 0.5 3.375C0.166667 3.04167 0 2.64583 0 2.1875C0 1.72917 0.166667 1.33333 0.5 1C0.833333 0.666667 1.22917 0.5 1.6875 0.5C2.14583 0.5 2.54167 0.666667 2.875 1C3.20833 1.33333 3.375 1.72917 3.375 2.1875C3.375 2.64583 3.20833 3.04167 2.875 3.375ZM14 14.5H11.0938V9.9375C11.0938 9.58333 11.0833 9.30208 11.0625 9.09375C11.0417 8.86458 10.9896 8.61458 10.9062 8.34375C10.8229 8.05208 10.6667 7.83333 10.4375 7.6875C10.2292 7.54167 9.94792 7.46875 9.59375 7.46875C8.90625 7.46875 8.4375 7.6875 8.1875 8.125C7.95833 8.5625 7.84375 9.14583 7.84375 9.875V14.5H4.9375V5.15625H7.75V6.4375H7.78125C7.98958 6.02083 8.33333 5.66667 8.8125 5.375C9.29167 5.0625 9.86458 4.90625 10.5312 4.90625C11.2396 4.90625 11.8229 5.02083 12.2812 5.25C12.7604 5.45833 13.1146 5.78125 13.3438 6.21875C13.5938 6.63542 13.7604 7.09375 13.8438 7.59375C13.9479 8.09375 14 8.6875 14 9.375V14.5Z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer--container__wrapper w-full md:w-1/2 lg:w-[22%]">
+        <h5 class="footer--title">Explore</h5>
+        <div
+          v-for="(item, i) in menuExplore"
+          :key="`menu-${i}`"
+          class="footer--item"
+        >
+          <div class="footer--item__point">
+            <div
+              v-for="i in 3"
+              :key="`circle-${i}`"
+              class="footer--item__point--circle"
+            ></div>
+          </div>
+          <router-link :to="item.route">
+            <p>{{ item.name }}</p>
+          </router-link>
+        </div>
+      </div>
+      <div class="footer--container__wrapper w-full md:w-1/2 lg:w-[25%]">
+        <h5 class="footer--title">Utility Pages</h5>
+        <div
+          v-for="(item, i) in menuUtility"
+          :key="`menu-${i}`"
+          class="footer--item"
+        >
+          <div class="footer--item__point">
+            <div
+              v-for="i in 3"
+              :key="`circle-${i}`"
+              class="footer--item__point--circle"
+            ></div>
+          </div>
+          <router-link :to="item.route">
+            <p>{{ item.name }}</p>
+          </router-link>
+        </div>
+      </div>
+      <div class="footer--container__wrapper w-full md:w-1/2 lg:w-[30%]">
+        <h5 class="footer--title">Keep in Touch</h5>
+        <div
+          v-for="(item, i) in menuContact"
+          :key="`contact-${i}`"
+          class="footer--contact"
+        >
+          <h5 class="footer--contact__title">{{ item.title }}:</h5>
+          <p class="footer--contact__desc">
+            {{ item.desc }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="footer--copyright">
+      <p>
+        © Drafted by <span>Victorflow</span> - Powered by <span>Webflow</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      menuExplore: [
+        {
+          id: 1,
+          name: "Home",
+          route: "/",
+        },
+        {
+          id: 2,
+          name: "About Us",
+          route: "/about",
+        },
+        {
+          id: 3,
+          name: "Services",
+          route: "/services",
+        },
+        {
+          id: 4,
+          name: "Appointments",
+          route: "/appointments",
+        },
+        {
+          id: 5,
+          name: "Blog",
+          route: "/blog",
+        },
+        {
+          id: 6,
+          name: "Contact Us",
+          route: "/contact",
+        },
+      ],
+      menuUtility: [
+        {
+          id: 1,
+          name: "Start Here",
+          route: "/",
+        },
+        {
+          id: 2,
+          name: "Style Guide",
+          route: "/",
+        },
+        {
+          id: 3,
+          name: "404 Not Found",
+          route: "/",
+        },
+        {
+          id: 4,
+          name: "Password Protected",
+          route: "/",
+        },
+        {
+          id: 5,
+          name: "Licenses",
+          route: "/",
+        },
+        {
+          id: 6,
+          name: "Changelog",
+          route: "/",
+        },
+      ],
+      menuContact: [
+        {
+          id: 1,
+          title: "Address",
+          desc: "24A Kingston St. Los Vegas NC 28202, USA",
+        },
+        {
+          id: 2,
+          title: "Mail",
+          desc: "support@pages.com",
+        },
+        {
+          id: 3,
+          title: "Phone",
+          desc: "(+22) 123 - 456 - 789",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,317 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-container">
+      <div class="navbar-left">
+        <img src="@/assets/img/logo/logo.png" alt="Logo" />
+
+        <!-- SOCMED -->
+        <div class="navbar-socmed">
+          <div class="socmed socmed-bg-white">
+            <svg
+              width="10"
+              height="17"
+              viewBox="0 0 10 17"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.71875 9.5H6.375V16.5H3.25V9.5H0.71875V6.59375H3.25V4.40625C3.25 3.15625 3.59375 2.19792 4.28125 1.53125C4.96875 0.84375 5.88542 0.5 7.03125 0.5C7.36458 0.5 7.71875 0.520833 8.09375 0.5625C8.46875 0.583333 8.76042 0.614583 8.96875 0.65625L9.28125 0.6875V3.15625H8.03125C7.44792 3.15625 7.02083 3.3125 6.75 3.625C6.5 3.91667 6.375 4.28125 6.375 4.71875V6.59375H9.15625L8.71875 9.5Z"
+              />
+            </svg>
+          </div>
+          <div class="socmed socmed-bg-white">
+            <svg
+              width="16"
+              height="13"
+              viewBox="0 0 16 13"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.3438 3.25C14.3646 3.33333 14.375 3.46875 14.375 3.65625C14.375 4.76042 14.1667 5.86458 13.75 6.96875C13.3333 8.05208 12.7396 9.05208 11.9688 9.96875C11.2188 10.8646 10.2396 11.5938 9.03125 12.1562C7.82292 12.7188 6.48958 13 5.03125 13C3.19792 13 1.52083 12.5104 0 11.5312C0.229167 11.5521 0.489583 11.5625 0.78125 11.5625C2.30208 11.5625 3.66667 11.0938 4.875 10.1562C4.14583 10.1562 3.5 9.94792 2.9375 9.53125C2.39583 9.09375 2.02083 8.55208 1.8125 7.90625C2.02083 7.92708 2.21875 7.9375 2.40625 7.9375C2.69792 7.9375 2.98958 7.90625 3.28125 7.84375C2.53125 7.67708 1.90625 7.29167 1.40625 6.6875C0.90625 6.08333 0.65625 5.39583 0.65625 4.625V4.5625C1.11458 4.83333 1.60417 4.97917 2.125 5C1.14583 4.33333 0.65625 3.41667 0.65625 2.25C0.65625 1.66667 0.8125 1.11458 1.125 0.59375C1.9375 1.61458 2.92708 2.42708 4.09375 3.03125C5.28125 3.63542 6.54167 3.96875 7.875 4.03125C7.83333 3.78125 7.8125 3.53125 7.8125 3.28125C7.8125 2.38542 8.125 1.61458 8.75 0.96875C9.39583 0.322917 10.1667 0 11.0625 0C12.0208 0 12.8229 0.34375 13.4688 1.03125C14.2188 0.885417 14.9167 0.625 15.5625 0.25C15.3125 1.02083 14.8333 1.625 14.125 2.0625C14.75 1.97917 15.375 1.80208 16 1.53125C15.5417 2.19792 14.9896 2.77083 14.3438 3.25Z"
+              />
+            </svg>
+          </div>
+          <div class="socmed socmed-bg-white">
+            <svg
+              width="14"
+              height="15"
+              viewBox="0 0 14 15"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.125 14.5H0.21875V5.15625H3.125V14.5ZM2.875 3.375C2.54167 3.70833 2.14583 3.875 1.6875 3.875C1.22917 3.875 0.833333 3.70833 0.5 3.375C0.166667 3.04167 0 2.64583 0 2.1875C0 1.72917 0.166667 1.33333 0.5 1C0.833333 0.666667 1.22917 0.5 1.6875 0.5C2.14583 0.5 2.54167 0.666667 2.875 1C3.20833 1.33333 3.375 1.72917 3.375 2.1875C3.375 2.64583 3.20833 3.04167 2.875 3.375ZM14 14.5H11.0938V9.9375C11.0938 9.58333 11.0833 9.30208 11.0625 9.09375C11.0417 8.86458 10.9896 8.61458 10.9062 8.34375C10.8229 8.05208 10.6667 7.83333 10.4375 7.6875C10.2292 7.54167 9.94792 7.46875 9.59375 7.46875C8.90625 7.46875 8.4375 7.6875 8.1875 8.125C7.95833 8.5625 7.84375 9.14583 7.84375 9.875V14.5H4.9375V5.15625H7.75V6.4375H7.78125C7.98958 6.02083 8.33333 5.66667 8.8125 5.375C9.29167 5.0625 9.86458 4.90625 10.5312 4.90625C11.2396 4.90625 11.8229 5.02083 12.2812 5.25C12.7604 5.45833 13.1146 5.78125 13.3438 6.21875C13.5938 6.63542 13.7604 7.09375 13.8438 7.59375C13.9479 8.09375 14 8.6875 14 9.375V14.5Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <!-- NAVBAR RIGHT DESKTOP -->
+      <div class="navbar-right navbar-right-desktop">
+        <div class="navbar-right-desktop__item">
+          <nuxt-link :to="'/'">
+            <p
+              class="navbar-item__text"
+              :class="{ 'navbar-item__text--active': isActiveMenu('/') }"
+            >
+              Home
+            </p>
+          </nuxt-link>
+          <div class="relative cursor-pointer">
+            <div
+              class="navbar-item__dropdown"
+              @mouseenter="pagesDropdown = true"
+            >
+              <p
+                class="navbar-item__text"
+                :class="{
+                  'navbar-item__text--active':
+                    isActiveMenu('/store') || isActiveMenu('/blog'),
+                }"
+              >
+                Pages
+              </p>
+              <img
+                src="@/assets/img/icon/chevron.svg"
+                alt="Chevron"
+                class="navbar-item__dropdown--chevron"
+              />
+            </div>
+            <div
+              class="navbar-dropdown"
+              :class="{
+                'opacity-0': !pagesDropdown,
+                'opacity-100': pagesDropdown,
+              }"
+              v-if="pagesDropdown"
+              @mouseleave="pagesDropdown = false"
+            >
+              <nuxt-link :to="'/store'">
+                <p
+                  class="navbar-dropdown__item"
+                  :class="{
+                    'navbar-dropdown__item--active': isActiveMenu('/store'),
+                  }"
+                >
+                  Store
+                </p>
+              </nuxt-link>
+              <nuxt-link :to="'/blog'">
+                <p
+                  class="navbar-dropdown__item"
+                  :class="{
+                    'navbar-dropdown__item--active': isActiveMenu('/blog'),
+                  }"
+                >
+                  Blog
+                </p>
+              </nuxt-link>
+            </div>
+          </div>
+          <nuxt-link :to="'/about'">
+            <p
+              class="navbar-item__text"
+              :class="{ 'navbar-item__text--active': isActiveMenu('/about') }"
+            >
+              About
+            </p>
+          </nuxt-link>
+          <nuxt-link :to="'/services'">
+            <p
+              class="navbar-item__text"
+              :class="{
+                'navbar-item__text--active': isActiveMenu('/services'),
+              }"
+            >
+              Services
+            </p>
+          </nuxt-link>
+          <nuxt-link :to="'/contact'">
+            <p
+              class="navbar-item__text"
+              :class="{ 'navbar-item__text--active': isActiveMenu('/contact') }"
+            >
+              Contact
+            </p>
+          </nuxt-link>
+        </div>
+        <div class="navbar-right--cart">
+          <img src="@/assets/img/icon/cart.svg" alt="Cart" />
+          <div class="navbar-right--cart__sup">01</div>
+        </div>
+        <div class="button button-bg-yellow">
+          <p>Order Today</p>
+        </div>
+      </div>
+
+      <div class="navbar-right navbar-right-mobile">
+        <div class="navbar-right--cart">
+          <img src="@/assets/img/icon/cart.svg" alt="Cart" />
+          <div class="navbar-right--cart__sup">01</div>
+        </div>
+        <button
+          class="burger-container"
+          :class="{
+            'text-black': !isBurgerMenuDown,
+            'text-white': isBurgerMenuDown,
+          }"
+          @click="isBurgerMenuDown = !isBurgerMenuDown"
+        >
+          <div class="burger-container--wrapper">
+            <span
+              aria-hidden="true"
+              class="burger-container--wrapper__span"
+              :class="{
+                'rotate-45': isBurgerMenuDown,
+                ' -translate-y-1.5': !isBurgerMenuDown,
+              }"
+            ></span>
+            <span
+              aria-hidden="true"
+              class="burger-container--wrapper__span"
+              :class="{ 'opacity-0': isBurgerMenuDown }"
+            ></span>
+            <span
+              aria-hidden="true"
+              class="burger-container--wrapper__span"
+              :class="{
+                '-rotate-45': isBurgerMenuDown,
+                ' translate-y-1.5': !isBurgerMenuDown,
+              }"
+            ></span>
+          </div>
+        </button>
+      </div>
+    </div>
+
+    <!-- NAVBAR BURGER -->
+    <div
+      v-if="isBurgerMenuDown"
+      class="navbar-burger"
+      :class="{
+        'h-[100vh] opacity-100': isBurgerMenuDown,
+        'h-0 opacity-0': !isBurgerMenuDown,
+      }"
+    >
+      <template>
+        <router-link :to="'/'">
+          <div class="navbar-burger--item">
+            <p>Home</p>
+          </div>
+        </router-link>
+        <!-- <router-link :to="'/'"> -->
+        <div
+          class="navbar-burger--item"
+          @click="pagesMenuBurger = !pagesMenuBurger"
+        >
+          <div class="flex justify-between items-center">
+            <p>Pages</p>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+              class="-rotate-90 stroke-primary-two transition-all duration-500"
+              :class="{ 'rotate-0': pagesMenuBurger }"
+            >
+              <path
+                d="M4 6L8 10L12 6"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <div v-if="pagesMenuBurger" class="navbar-burger--item__child">
+            <router-link :to="'/store'">
+              <p>Store</p>
+            </router-link>
+            <router-link :to="'/'">
+              <p>Blog</p>
+            </router-link>
+          </div>
+        </div>
+        <!-- </router-link> -->
+        <router-link :to="'/'">
+          <div class="navbar-burger--item">
+            <p>About</p>
+          </div>
+        </router-link>
+        <router-link :to="'/'">
+          <div class="navbar-burger--item">
+            <p>Service</p>
+          </div>
+        </router-link>
+        <router-link :to="'/'">
+          <div class="navbar-burger--item">
+            <p>Contact</p>
+          </div>
+        </router-link>
+        <div class="mt-4 button button-bg-yellow">
+          <p>Order Today</p>
+        </div>
+        <!-- SOCMED -->
+        <div class="navbar-socmed--mobile mt-4">
+          <div class="socmed socmed-bg-white">
+            <svg
+              width="10"
+              height="17"
+              viewBox="0 0 10 17"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.71875 9.5H6.375V16.5H3.25V9.5H0.71875V6.59375H3.25V4.40625C3.25 3.15625 3.59375 2.19792 4.28125 1.53125C4.96875 0.84375 5.88542 0.5 7.03125 0.5C7.36458 0.5 7.71875 0.520833 8.09375 0.5625C8.46875 0.583333 8.76042 0.614583 8.96875 0.65625L9.28125 0.6875V3.15625H8.03125C7.44792 3.15625 7.02083 3.3125 6.75 3.625C6.5 3.91667 6.375 4.28125 6.375 4.71875V6.59375H9.15625L8.71875 9.5Z"
+              />
+            </svg>
+          </div>
+          <div class="socmed socmed-bg-white">
+            <svg
+              width="16"
+              height="13"
+              viewBox="0 0 16 13"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.3438 3.25C14.3646 3.33333 14.375 3.46875 14.375 3.65625C14.375 4.76042 14.1667 5.86458 13.75 6.96875C13.3333 8.05208 12.7396 9.05208 11.9688 9.96875C11.2188 10.8646 10.2396 11.5938 9.03125 12.1562C7.82292 12.7188 6.48958 13 5.03125 13C3.19792 13 1.52083 12.5104 0 11.5312C0.229167 11.5521 0.489583 11.5625 0.78125 11.5625C2.30208 11.5625 3.66667 11.0938 4.875 10.1562C4.14583 10.1562 3.5 9.94792 2.9375 9.53125C2.39583 9.09375 2.02083 8.55208 1.8125 7.90625C2.02083 7.92708 2.21875 7.9375 2.40625 7.9375C2.69792 7.9375 2.98958 7.90625 3.28125 7.84375C2.53125 7.67708 1.90625 7.29167 1.40625 6.6875C0.90625 6.08333 0.65625 5.39583 0.65625 4.625V4.5625C1.11458 4.83333 1.60417 4.97917 2.125 5C1.14583 4.33333 0.65625 3.41667 0.65625 2.25C0.65625 1.66667 0.8125 1.11458 1.125 0.59375C1.9375 1.61458 2.92708 2.42708 4.09375 3.03125C5.28125 3.63542 6.54167 3.96875 7.875 4.03125C7.83333 3.78125 7.8125 3.53125 7.8125 3.28125C7.8125 2.38542 8.125 1.61458 8.75 0.96875C9.39583 0.322917 10.1667 0 11.0625 0C12.0208 0 12.8229 0.34375 13.4688 1.03125C14.2188 0.885417 14.9167 0.625 15.5625 0.25C15.3125 1.02083 14.8333 1.625 14.125 2.0625C14.75 1.97917 15.375 1.80208 16 1.53125C15.5417 2.19792 14.9896 2.77083 14.3438 3.25Z"
+              />
+            </svg>
+          </div>
+          <div class="socmed socmed-bg-white">
+            <svg
+              width="14"
+              height="15"
+              viewBox="0 0 14 15"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.125 14.5H0.21875V5.15625H3.125V14.5ZM2.875 3.375C2.54167 3.70833 2.14583 3.875 1.6875 3.875C1.22917 3.875 0.833333 3.70833 0.5 3.375C0.166667 3.04167 0 2.64583 0 2.1875C0 1.72917 0.166667 1.33333 0.5 1C0.833333 0.666667 1.22917 0.5 1.6875 0.5C2.14583 0.5 2.54167 0.666667 2.875 1C3.20833 1.33333 3.375 1.72917 3.375 2.1875C3.375 2.64583 3.20833 3.04167 2.875 3.375ZM14 14.5H11.0938V9.9375C11.0938 9.58333 11.0833 9.30208 11.0625 9.09375C11.0417 8.86458 10.9896 8.61458 10.9062 8.34375C10.8229 8.05208 10.6667 7.83333 10.4375 7.6875C10.2292 7.54167 9.94792 7.46875 9.59375 7.46875C8.90625 7.46875 8.4375 7.6875 8.1875 8.125C7.95833 8.5625 7.84375 9.14583 7.84375 9.875V14.5H4.9375V5.15625H7.75V6.4375H7.78125C7.98958 6.02083 8.33333 5.66667 8.8125 5.375C9.29167 5.0625 9.86458 4.90625 10.5312 4.90625C11.2396 4.90625 11.8229 5.02083 12.2812 5.25C12.7604 5.45833 13.1146 5.78125 13.3438 6.21875C13.5938 6.63542 13.7604 7.09375 13.8438 7.59375C13.9479 8.09375 14 8.6875 14 9.375V14.5Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      pagesDropdown: false,
+      isBurgerMenuDown: false,
+      pagesMenuBurger: false,
+    };
+  },
+  methods: {
+    isActiveMenu(link) {
+      let currUrl = this.$route.fullPath;
+
+      if (currUrl === link) {
+        return true;
+      }
+      return false;
+    },
+  },
+};
+</script>
